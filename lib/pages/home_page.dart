f916e26e01c5c5bd6b31a9e47c9c068124a27f7a
Jenica B Sarasam <am.sc.u4cse23323@am.students.amrita.edu>
2025-01-31 22:57:40 +0530

    Settings page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/components/my_current_location.dart';
 import 'package:food_delivery/components/my_drawer.dart';
+import 'package:food_delivery/components/my_silver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,9 +14,27 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home")),
         drawer: MyDrawer(),
+        body: NestedScrollView(
+          headerSliverBuilder:(context,innerBoxisScrolled)=>[
+            MySilverAppBar(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Divider(
+                    indent: 25,
+                    endIndent: 25,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  //my current location
+                  MyCurrentLocation()
+                ],
+              ), 
+              title: Text('title'),
+              ),
+          ],
+          body: Container(color: Colors.blue,),
+        ),
     );
   }
 }
